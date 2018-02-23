5ac8fbfeee57ba6e528e9bad6230e6611b701cca
Pato <patonyango@hotmail.com>
2018-02-23 18:03:08 +0300

    adding pdf styling

--- index.js
+++ index.js
@@ -6,10 +6,143 @@ var specialElementHandlers = {
 };
 
 $('#cmd').click(function () {
-    doc.fromHTML($('#content').html(), 15, 15, {
+
+    doc.fromHTML($('.top #line1').html(), 80, 15, {
         'width': 170,
             'elementHandlers': specialElementHandlers
     });
+
+    doc.line(80, 20, 123, 20)
+
+
+
+    doc.fromHTML($('#line2').html(), 35, 25, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.line(35, 30, 168, 30)
+
+
+    doc.fromHTML($('#line3').html(), 60, 35, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.line(60, 40, 135, 40)
+
+    doc.fromHTML($('#line4').html(), 90, 45, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.line(90, 50, 112, 50)
+
+    doc.fromHTML($('.declaration').html(), 15, 60, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+    doc.fromHTML($('#sworn').html(), 15, 130, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.fromHTML($('.location').html(), 32, 130, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.fromHTML($('#by').html(), 60, 130, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.fromHTML($('#bracket1').html(), 100, 130, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.fromHTML($('.name').html(), 15, 140, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.fromHTML($('#bracket2').html(), 100, 140, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    //insert line
+    doc.line(140, 150, 175, 150)
+
+    doc.fromHTML($('#this').html(), 15, 150, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.fromHTML($('#day').html(), 40, 150, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.fromHTML($('#20').html(), 80, 150, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.fromHTML($('#bracket7').html(), 100, 150, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.fromHTML($('#deponent').html(), 150, 150, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.fromHTML($('#before').html(), 15, 160, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.fromHTML($('#bracket4').html(), 100, 160, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.fromHTML($('#bracket5').html(), 100, 170, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.fromHTML($('#bracket6').html(), 100, 180, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.fromHTML($('#bracket7').html(), 100, 190, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.fromHTML($('#stampspace').html(), 15, 200, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.line(15, 205, 72, 205)
+
+
+    doc.fromHTML($('#bracket7').html(), 100, 200, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
+    doc.fromHTML($('#footnote').html(), 15, 270, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+
     doc.save('affidavit.pdf');
 });
 
